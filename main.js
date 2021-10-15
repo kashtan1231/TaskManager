cf18c9e7097b327b5de1b9ec94e525fb3d190f0e
kashtan1231 <67584196+kashtan1231@users.noreply.github.com>
2021-10-15 17:55:48 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,8 +6,11 @@ const addTask = document.querySelector(".addTask");
 const popIt = document.querySelector(".popIt");
 const cancer = document.querySelector(".cancer");
 const apply = document.querySelector(".apply");
+const attention = document.querySelector(".attention");
 const fogging = document.querySelector(".fogging");
+const filter = document.querySelector(".filter");
 const tasks = [];
+const users = [];
 let currentTask;
 
 const renderTasks = () => {
@@ -48,10 +51,22 @@ const getChildElementByName = (parent, name, nth = 1) => {
   return null;
 };
 
-
 addTask.addEventListener("click", () => {
+  const name = document.querySelector(".nameNew");
+  const date = document.querySelector(".dateNew");
+  const user = document.querySelector(".userNew");
+  const description = document.querySelector(".descriptionNew");
+  const state = document.querySelector(".stateNew");
+
+  name.value = null;
+  date.value = null;
+  user.value = null;
+  description.value = null;
+  state.value = null;
+
   popIt.style.display = "block";
   fogging.style.display = "block";
+  attention.style.display = "none";
 });
 
 cancer.addEventListener("click", () => {
@@ -68,36 +83,51 @@ apply.addEventListener("click", (event) => {
 
   if (currentTask) {
     const task = document.getElementById(currentTask);
-    getChildElementByName(task, 'h3').innerHTML = name.value;
-    const content = getChildElementByClass(task, 'content');
-    getChildElementByName(content, 'p').innerHTML = date.value;
-    getChildElementByName(content, 'p', 2).innerHTML = user.value;
-    const desc = getChildElementByClass(task, 'desc');
-    getChildElementByName(desc, 'p').innerHTML = description.value;
-    const stateDiv = getChildElementByClass(task, 'state');
-    getChildElementByName(stateDiv, 'p').innerHTML = state.value;
-    
+    getChildElementByName(task, "h3").innerHTML = name.value;
+    const content = getChildElementByClass(task, "content");
+    getChildElementByName(content, "p").innerHTML = date.value;
+    getChildElementByName(content, "p", 2).innerHTML = user.value;
+    const desc = getChildElementByClass(task, "desc");
+    getChildElementByName(desc, "p").innerHTML = description.value;
+    const stateDiv = getChildElementByClass(task, "state");
+    getChildElementByName(stateDiv, "p").innerHTML = state.value;
+
     popIt.style.display = "none";
     fogging.style.display = "none";
     currentTask = null;
     return;
   }
+  if (name.value && date.value && user.value && state.value !== "Сталин") {
+    const task = createNewTask(
+      name.value,
+      date.value,
+      user.value,
+      description.value,
+      state.value,
+      `${tasks.length}`
+    );
+    
+    users.push(user);
+for (const castomer of users) {
+  if (castomer.value !== user.value) {
+    const filUser = document.createElement("option");
+    filUser.innerHTML = user.value;
+    filter.appendChild(filUser);
+    
+  }
+}
+   
 
-  const task = createNewTask(
-    name.value,
-    date.value,
-    user.value,
-    description.value,
-    state.value,
-    `${tasks.length}`
-  );
 
-  tasks.push(task);
-  renderTasks();
-  popIt.style.display = "none";
-  fogging.style.display = "none";
-  currentTask = null;
-  return;
+    tasks.push(task);
+    renderTasks();
+    popIt.style.display = "none";
+    fogging.style.display = "none";
+    currentTask = null;
+    return;
+  } else {
+    attention.style.display = "block";
+  }
 });
 
 input.addEventListener("input", () => {
@@ -105,14 +135,17 @@ input.addEventListener("input", () => {
   const items = document.querySelectorAll(".newTask");
   if (val) {
     for (const item of items) {
-      if (getChildElementByName(item, 'h3').innerText.search(val) == -1) {
+      if (getChildElementByName(item, "h3").innerText.search(val) == -1) {
         item.classList.add("hide");
       } else {
         item.classList.remove("hide");
       }
     }
     return;
+  } else {
+    for (const item of items) {
+      item.classList.remove("hide");
+    }
   }
   renderTasks();
 });
-
